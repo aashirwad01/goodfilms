e5523c20f673130ad5272e33c4de6a91e9d775f9
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2021-09-30 02:48:01 +0530

    feat-added movie descriptions

--- src/App.js
+++ src/App.js
@@ -4,79 +4,79 @@ import './App.css';
 
 const MovieDB = {
   SciFi: [
-    { name: "Arrival", rating: "5/5" },
-    { name: "2001:A Space Odyssey", rating: "5/5" },
-    { name: "Inception", rating: "4/5" },
-    { name: "Donnie Darko", rating: "4/5" },
-    { name: "Back to the Future Triology", rating: "4/5" }
+    { name: "Arrival", rating: "5/5",desc:"I love it for visuals.I consider it to be 2001 Space Odyssey of our generation." },
+    { name: "2001:A Space Odyssey", rating: "5/5",desc:"Need no describing .A movie which is still celebrated and my current favourite." },
+    { name: "Inception", rating: "4/5",desc:"A Nolan Masterpiece about Dream Layers.Exceptional Visuals and Acting ." },
+    { name: "Donnie Darko", rating: "4/5",desc:"A SciFi Thriller MindFuck movie . You won't understand it once." },
+    { name: "Back to the Future Triology", rating: "4/5",desc: "Most fun SciFI movie ever.The no of alternate timelines inspiration given by this movie is unimaginable."}
 
   ],
 
   Fantasy: [
     {
       name: "Lord of the Rings Trilogy",
-      rating: "5/5"
+      rating: "5/5",desc:"A Masterpiece Fantasy story telling. Cinematography , Acting direction peak."
     },
     {
       name: "Harry Potter Series",
-      rating: "4.5/5"
+      rating: "4.5/5",desc:"We grew with Harry growing and loathed Voldemort.Need i say more to intice you in watching this gem series."
     },
     {
       name: "Lal Kaptaan",
-      rating: "4/5"
+      rating: "4/5",desc:"A Brilliant movie underrrated gem based on History and Fantasy very unlike Bollywood."
     },
-    { name: "Hobbit Triology", rating: "3.75/5" }
+    { name: "Hobbit Triology", rating: "3.75/5",desc:"A prequel to Lord of rings Trilogy.Though not as brilliant but stands on apart." }
 
   ],
   Crime: [
     {
       name: "The Godfather Trilogy",
-      rating: "4.5/5"
+      rating: "4.5/5",desc:"The legendary duo of Al Pacino and Robert Di Neiro in epic story of GangSters of NewYork based on true inspirations."
     },
     {
       name: "Goodfellas",
-      rating: "4.5/5"
+      rating: "4.5/5",desc:"This is widely considered to be best gangster movie ever. Can't argue against this."
     },
     {
       name: "Heat",
-      rating: "4/5"
+      rating: "4/5",desc:"AWatch it for Acting lessons. Al Pacino and Robert De Neiro in ultimate best in this crime thriller."
     },
     {
       name: "The Departed",
-      rating: "4/5"
+      rating: "4/5",desc:"Modern masterpiece with Jack Nicolson and based lesser known Irish Mobs."
     }
   ],
   Thriller: [
-    { name: "The Usual Suspects", rating: "4.5/5" },
-    { name: "Andhadhun", rating: "4/5" },
-    { name: "Drishyam", rating: "4/5" },
-    { name: "Prisoners", rating: "4/5" },
-    { name: "Memento", rating: "4/5" },
-    { name: "The Sixth Sense", rating: "3.75/5" }
+    { name: "The Usual Suspects", rating: "4.5/5",desc:"Ending will blow you apart." },
+    { name: "Andhadhun", rating: "4/5",desc:"AA wonderful performance by Tabbu and Ayushmann with gripping story." },
+    { name: "Drishyam", rating: "4/5",desc:"A world class cinema experience easily in best movie made in Indian Cinema ever." },
+    { name: "Prisoners", rating: "4/5",desc:"Visuals,Cinematography and Acting are impeccable.Best movie of Jake Gryllenhal in my views." },
+    { name: "Memento", rating: "4/5",desc:"A nolan Masterpiece. Movie making at peak." },
+    { name: "The Sixth Sense", rating: "3.75/5",desc:"A classic keeping your guessing and boom a twist thriller." }
     
 
 
   ],
 
   SuperHero: [
-    { name: "The Batman Trilogy", rating: "4.5/5" },
-    { name: "Logan", rating: "4/5" },
-    { name: "Avengers", rating: "3.75/5" },
-    { name: "Mard ko Dard nahi Hota", rating: "3.5/5" },
-    { name: "Wonder Woman", rating: "3.5/5" }
+    { name: "The Batman Trilogy", rating: "4.5/5" ,desc:"This heralded the true era of superhero masterpiece movies.Must must watch. "},
+    { name: "Logan", rating: "4/5",desc:"Best superhero movie of decade.Reinvented the genre after Dark Knight." },
+    { name: "Avengers", rating: "3.75/5",desc:"A spectacle saga of decade . A storytelling and planning of Kevin Feige making Marvel immortal. " },
+    { name: "Mard ko Dard nahi Hota", rating: "3.5/5",desc:"very underrated bollywood superhero movie . Highly Enjoyable." },
+    { name: "Wonder Woman", rating: "3.5/5",desc:"DC reincarnation." }
 
 
   ],
   BioPic: [
-    { name: "Lincoln", rating: "5/5" },
-    { name: "Downfall", rating: "5/5" },
-    { name: "The King's Speech", rating: "4/5" },
-    { name: "The Irishman", rating: "4/5" },
-    { name: "The Big Short", rating: "4/5" },
-    { name: "Darkest Hour", rating: "3.75/5" },
-    { name: "Pele :Birth of a Legend", rating: "3.75/5" },
+    { name: "Lincoln", rating: "5/5",desc:"The man The Myth The Legend Daniel Day Lewis . What a movie and what a story." },
+    { name: "Downfall", rating: "5/5" ,desc:"Again a brilliant movie on true events in German. True flagbearer of world class german cinema."},
+    { name: "The King's Speech", rating: "4/5",desc:"Oscar winning Performance enhanced by pitch perfect feel good struggle story." },
+    { name: "The Irishman", rating: "4/5" ,desc:"GangSter Biopic MasterPiece by Scorcese.It's an experience."},
+    { name: "The Big Short", rating: "4/5" ,desc:"A true story enhanced by exceptional acting and accurate storytelling on 2008 financial crisis."},
+    { name: "Darkest Hour", rating: "3.75/5" ,desc:"Edge of seat historical thriller."},
+    { name: "Pele :Birth of a Legend", rating: "3.75/5",desc:"Literally made me cry. A must watch." },
    
-    { name: "Imitation Game", rating: "3.5/5" }
+    { name: "Imitation Game", rating: "3.5/5",desc:"The man behind computer Alan Turing's Biopic." }
 
 
   ]
@@ -123,6 +123,7 @@ function genreClickHandler(genre){
             >
               <div   style={{fontSize:"larger",color:"black",backgroundColor:"lightcyan"}}>{mkv.name}</div>
               <div style={{color:"rgb(9, 53, 53)",backgroundColor:"lightcyan"}}><small   >{mkv.rating}</small></div>
+              <div style={{color:"rgb(9, 53, 53)",backgroundColor:"lightcyan"}}><small   >{mkv.desc}</small></div>
             </li>
           ))}
 
